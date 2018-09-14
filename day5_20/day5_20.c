74ba6bf086b3310eb775198b17d496cd0fa60170
Gentment <1844861420@qq.com>
2018-09-13 23:26:35 -0800

    new file:   day5_20/day5_20.c 	new file:   day5_20/mm.c

---
+++ day5_20/day5_20.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<mm.c>
+int main()
+{
+int a = 1;
+int b = 2;
+int c = max(a,b);
+printf("%d\n",c);
+}
---
+++ day5_20/mm.c
@@ -0,0 +1,8 @@
+int max(int x ,int y)
+{
+  if(x>y){
+  return x;
+  }else
+  if(x<y){
+ return y;}
+}
