0d28f0ae0511034ff4270e7aa1be292b082d451a
Liuxiaokun <1844861420@qq.com>
2018-09-17 21:24:52 +0800

    new file:   day9_16/.vs/day9_16/v15/.suo new file:   day9_16/.vs/day9_16/v15/Browse.VC.db new file:   day9_16/.vs/day9_16/v15/ipch/AutoPCH/8568cf1cd6416bfc/LEETCODE.ipch 	new file:   day9_16/Debug/day9_16.exe new file:   day9_16/Debug/day9_16.ilk 	new file:   day9_16/Debug/day9_16.pdb 	new file:   day9_16/day9_16.sln 	new file:   day9_16/day9_16/Debug/day9_16.log 	new file:   day9_16/day9_16/Debug/day9_16.tlog/CL.command.1.tlog 	new file:   day9_16/day9_16/Debug/day9_16.tlog/CL.read.1.tlog 	new file:   day9_16/day9_16/Debug/day9_16.tlog/CL.write.1.tlog 	new file:   day9_16/day9_16/Debug/day9_16.tlog/day9_16.lastbuildstate 	new file:   day9_16/day9_16/Debug/day9_16.tlog/link.command.1.tlog 	new file:   day9_16/day9_16/Debug/day9_16.tlog/link.read.1.tlog 	new file:   day9_16/day9_16/Debug/day9_16.tlog/link.write.1.tlog 	new file:   day9_16/day9_16/Debug/leetcode.obj 	new file:   day9_16/day9_16/Debug/vc141.idb 	new file:   day9_16/day9_16/Debug/vc141.pdb 	new file:   day9_16/day9_16/day9_16.vcxproj 	new file:   day9_16/day9_16/day9_16.vcxproj.filters 	new file:   day9_16/day9_16/day9_16.vcxproj.user 	new file:   day9_16/day9_16/leetcode.c 	new file:   day9_16/day9_16/test.cpp 	modified:   day9_11/.vs/day9_11/v15/.suo 	modified:   day9_11/.vs/day9_11/v15/Browse.VC.db 	modified:   day9_12_Sort/.vs/day9_12_Sort/v15/.suo 	modified:   day9_12_Sort/.vs/day9_12_Sort/v15/Browse.VC.db 	modified:   day9_12_Sort/.vs/day9_12_Sort/v15/ipch/AutoPCH/69cd1648daaf101e/TEST.ipch 	modified:   day9_12_Sort/Debug/day9_12_Sort.exe 	modified:   day9_12_Sort/Debug/day9_12_Sort.ilk 	modified:   day9_12_Sort/Debug/day9_12_Sort.pdb 	modified:   day9_12_Sort/day9_12_Sort (modified content, untracked content) 	modified:   day9_13/.vs/day9_13/v15/.suo 	modified:   day9_13/.vs/day9_13/v15/Browse.VC.db 	modified:   day9_13/Debug/day9_13.exe 	modified:   day9_13/Debug/day9_13.ilk 	modified:   day9_13/Debug/day9_13.pdb 	modified:   day9_13/day9_13/Debug/day9_13.tlog/CL.read.1.tlog 	modified:   day9_13/day9_13/Debug/day9_13.tlog/link.read.1.tlog 	modified:   day9_13/day9_13/Debug/test.obj 	modified:   day9_13/day9_13/Debug/vc141.idb 	modified:   day9_13/day9_13/Debug/vc141.pdb 	modified:   day9_13/day9_13/test.c 	modified:   day9_8hash/.vs/day9_8hash/v15/.suo 	modified:   day9_8hash/.vs/day9_8hash/v15/Browse.VC.db 	modified:   day9_8hash/.vs/day9_8hash/v15/ipch/AutoPCH/180840207f89d242/TEST.ipch 	modified:   day9_8hash/Debug/day9_8hash.exe 	modified:   day9_8hash/Debug/day9_8hash.ilk 	modified:   day9_8hash/Debug/day9_8hash.pdb 	modified:   day9_8hash/day9_8hash/Debug/day9_8hash.log 	modified:   day9_8hash/day9_8hash/Debug/day9_8hash.tlog/link.read.1.tlog 	modified:   day9_8hash/day9_8hash/Debug/day9_8hash.tlog/link.write.1.tlog 	modified:   day9_8hash/day9_8hash/Debug/test.obj 	modified:   day9_8hash/day9_8hash/Debug/vc141.idb 	modified:   day9_8hash/day9_8hash/Debug/vc141.pdb 	modified:   day9_8hash/day9_8hash/HashBucket.h 	modified:   "\351\223\276\350\241\250" (new commits) 	modified:   "\351\241\272\345\272\217\350\241\250" (new commits)

---
+++ day9_16/day9_16/leetcode.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<limits.h>
+
+
+
+//int mySqrt_binarysearch(int x)
+//{
+//	if (x <= 0)	return 0;
+//
+//	long res = x;
+//	while (res*res > x)
+//	{
+//		res = (res + x / res) / 2;
+//	}
+//
+//	return res;
+//}
+//
+//int main()
+//{
+//	int ret = mySqrt_binarysearch(10);
+//	printf("%d ",ret);
+//	return 0;
+//}
